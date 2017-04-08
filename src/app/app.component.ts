1023aed71ab3ff43052b2d904a86bb93ae8e77db
Tomasz Nastały <tomasz.nastaly@gmail.com>
2017-04-09 00:06:24 +0200

    angular2 binding - basic (and probably incorrect) form handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  currName = '';
+  currSurname = '';
+  currAge: number;
+  currCountry = '';
+  currPrice: number;
+  players = [];
+
+  addPlayer() {
+    console.log('ciabaraba');
+    this.players.push({'name': this.currName, 'surname': this.currSurname,
+                       'age': this.currAge, 'country': this.currCountry, 'price': this.currPrice});
+    console.log(this.players.length)
+    this.currName = '';
+    this.currSurname = '';
+    this.currAge = null;
+    this.currCountry = '';
+    this.currPrice = null;
+  }
+
+  sanitizeAge(event) {
+    console.log(event.target);
+    if (isNaN(event.target.value) || event.target.value === null || event.target.value == '') {
+      console.log('hejo');
+      console.log(event.target.defaultValue);
+      this.currAge = event.target.defaultValue;
+      event.target.value = event.target.defaultValue;
+      console.log(this.currAge);
+    } else {
+      this.currAge = event.target.value;
+      console.log(this.currAge);
+    }
+  }
+
+  sanitizePrice(event) {
+   console.log(event.target);
+   if (isNaN(event.target.value) || event.target.value === null || event.target.value == '') {
+      console.log('hejo');
+      console.log(event.target.defaultValue);
+      this.currPrice = event.target.defaultValue;
+      event.target.value = event.target.defaultValue;
+      console.log(this.currPrice);
+    } else {
+      this.currPrice = event.target.value;
+      console.log(this.currPrice);
+      console.log(isNaN(this.currPrice));
+    }
+  }
+
+  checkIfReady() {
+    if(this.currName === '' || this.currSurname === '' || this.currCountry === '' ||
+    isNaN(this.currAge) || isNaN(this.currPrice) || this.currAge === null || this.currPrice === null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
